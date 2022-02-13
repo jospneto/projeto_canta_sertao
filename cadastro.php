2d4220019c47225e3458cbeac2c4327f93341211
Calleb Bezerra <calleb.bezerra@gmail.com>
2022-02-13 17:00:18 -0300

    Teste de login/senha com Banco de Dados
    Criado um sistema simples de login utilizando PHP e MySQL.
    
    É necessário ainda implementar esta solução na página principal do projeto.

---
+++ cadastro.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Crie sua conta</h1>
+
+    <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+
+  </body>
+</html>
---
+++ teste/cadastro.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Cadastro - Contratante</h1>
+    <form action="" method="POST">
+        <p>
+            <input type="text" name="nome_completo" placeholder="Nome completo">
+        </p>
+        <p>
+            <input type="text" name="nome_empresa" placeholder="Nome da Empresa">
+        </p>
+        <p>
+            <input type="email" name="email" placeholder="E-mail">
+        </p>
+        <p>
+            <input type="text" name="telefone" placeholder="Telefone">
+        </p>
+        <p>
+            <input type="text" name="cnpj" placeholder="CNPJ">
+        </p>
+        <p>
+            <input type="text" name="nome_usuario" placeholder="Nome de Usuário">
+        </p>
+        <p>
+            <input type="pasword" name="senha" placeholder="Senha">
+        </p>
+        <p>
+            <button type="submit">Cadastrar</button>
+        </p>
+    </form>
+</body>
+</html>
