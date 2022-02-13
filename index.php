64d4f1e6e9e2383b73af1ee9ee92488351162934
jospneto <josenetopereira380@gmail.com>
2022-02-13 15:45:13 -0300

    Atualização do código

--- index.php
+++ index.php
@@ -1,27 +1,37 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
-  </body>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Página Inicial</title>
+</head>
+<body>
+  <nav class="menu">
+    <img src="img/Logo.png">
+    <ul>
+      <li><a href=""></a>Contatos</li>
+      <li class="login"><a href=""></a>Login</li>
+      <li class="cadastro"><a href=""></a>Cadastro</li>
+    </ul>
+  </nav>
+  <div class="conteudo">
+    <h2>Procure artistas para seu <br> evento de forma rápida e simples</h2>
+    <ul>
+      <li><img src="/img/cadastro.png" alt=""></li>
+      <li><img src="img/gratis.png" alt=""></li>
+    </ul>
+  </div>
+  <footer class="rodape">
+    <h2>@copyCanta Sertão</h2>
+    <h2>Todos os direitos reservados</h2>
+    <ul>
+      <li><a href=""><img src="/img/WhatsApp.png" alt=""></a></li>
+      <li><a href=""><img src="/img/MSG.png" alt=""></a></li>
+      <li><a href=""><img src="/img/Instagram.png" alt=""></a></li>
+      <li><a href=""><img src="/img/Facebook.png" alt=""></a></li>
+    </ul>
+  </footer>
+</body>
 </html>
