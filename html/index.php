48ca0f539c3f66f93509f560e2f9c26f337f341c
jospneto <josenetopereira380@gmail.com>
2022-02-13 11:51:53 -0300

    atualização

---
+++ html/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Página Inicial</title>
+</head>
+<body>
+    <nav class="nav-search">
+        <img src="..img/logo.png"><p>Canta Sertão<p>
+        <ul>
+            <li><a href=""></a>Contato</li>
+            <li><a href="login"></a>Login</li>
+            <li><a href="cadastro"></a>Cadastre-se</li>
+        </ul>
+    </nav>
+    <div class="div-body">
+        <p>Procure artistas para seu evento de forma rápida e simples</p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus officiis quae ea quibusdam provident a consectetur! Temporibus sed earum repellat quae nam est. Facere nihil officia eum unde temporibus nam?
+    </div>
+    <footer>
+        <div>@copy Canta Sertão</div>
+        <p>Todos os direitos preservados</p>
+        <ul class="social-midias">
+            <li><a href=""></a>Whatsapp</li>
+            <li><a href=""></a>E-mail</li>
+            <li><a href=""></a>Instagram</li>
+            <li><a href=""></a>Facebook</li>
+        </ul>
+    </footer>
+</body>
+</html>
