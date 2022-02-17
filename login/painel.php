b8699d83a0f793bf5e803a24873201f4c3bd74d6
jospneto <josenetopereira380@gmail.com>
2022-02-17 00:26:11 -0300

    Atualizações

--- login/painel.php
+++ login/painel.php
@@ -1,7 +1,25 @@
-<?php
+<?php 
+    include('conexao.php');
 
-include('protect.php');
+    $msg = false;
+    if(isset($_FILES['imagem'])){
+        $imagem = $_FILES['imagem']['name'];
+        $extensao = strtolower(pathinfo($imagem, PATHINFO_EXTENSION));
 
+        $novo_nome = md5(time()).".".$extensao;
+
+        $diretorio = "imagens_bd/";
+
+        move_uploaded_file($_FILES['imagem']['tmp_name'], $diretorio . $novo_nome);
+
+        $sql_code = "INSERT INTO imagens_musico (idImagem, iMg) values (' ', '$novo_nome', NOW())";
+
+        if(mysqli_query($conexao, $sql_code)){
+            $msg = "Arquivo enviado com sucesso!";
+        }else{
+            $msg = "Falha ao enviar o arquivo!";
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,11 +27,34 @@ include('protect.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/stylePerfil.css">
     <title>Painel</title>
 </head>
 <body>
-    Bem vindo ao Painel, <?php echo $_SESSION['nome_fantasia']; ?>.
-
+    <nav class="menu">
+        <img src="/img/Logo.png">
+    </nav>
+    <div class="imagem">
+        <?php
+            if(isset($msg) && $msg != false){
+                echo "<p>$msg</p>";
+            }
+        ?>
+        <form action="painel.php" method="POST" enctype="multipart/form-data">
+            Selecione a imagem: <input type="file" name="imagem"/>
+            <br/>
+            <input type="submit" value="Enviar"/>
+	    </form>
+    </div>
+    <div class="perfil">
+        <h2><?php echo $_SESSION['nome_fantasia']; ?></h2>
+        <ul class="midias">
+        <li><a href=""><img src="/img/WhatsApp.png" alt=""></a></li>
+        <li><a href=""><img src="/img/msg.png" alt=""></a></li>
+        <li><a href=""><img src="/img/Instagram (2).png" alt=""></a></li>
+        <li><a href=""><img src="/img/Facebook (1).png" alt=""></a></li>
+        </ul>
+    </div>
     <p>
         <a href="logout.php">Sair</a>
     </p>
