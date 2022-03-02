51a4db5bdb72ebeb4f6c32bd43082452bde883e1
jospneto <josenetopereira380@gmail.com>
2022-03-02 12:01:43 -0300

    Atualĩzações

--- login/cadastro.php
+++ login/cadastro.php
@@ -1,44 +1,22 @@
-<?php
-    include("conexao.php");
-
-    $cpf_cnpj = $_POST['cpf_cnpj'];
-    $nome_fantasia = $_POST['nome_fantasia'];
-    $email = $_POST['email'];
-    $endereco = $_POST['endereco'];
-    $telefone = $_POST['telefone'];
-    $data_entrada = NOW();
-    $genero_musical = $_POST['genero_musical'];
-    $cache_show = $_POST['cache_show'];
-    $senha = md5($_POST['senha']);
-
-    $sql_code = "INSERT INTO user_musico values ('$cpf_cnpj', '$nome_fantasia', 
-    '$email', '$endereco', '$telefone', '$genero_musical', '$data_entrada', '$cache_show', '$senha')";
-    
-    if(mysqli_query($conexao, $sql_code)){
-        echo"Usuário cadastrado com sucesso!";
-    }else{
-        echo "Erro" . mysqli_connect_error($conexao);
-    }
-    mysqli_close($conexao);
-?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" href="/img/Logo.png">
-        <link rel="stylesheet" href="/css/style.css">
-        <title>Cadastro - Músico</title>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/img/Logo.png">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Login</title>
 
-        <!-- Principal CSS do Bootstrap -->
-        <link href="\bootstrap\dist\css\bootstrap.min.css" rel="stylesheet">
+    <!-- Principal CSS do Bootstrap -->
+    <link href="\bootstrap\dist\css\bootstrap.min.css" rel="stylesheet">
 
-        <!-- Estilos customizados para esse template -->
-        <link href="/css/signin.css" rel="stylesheet">
-    </head>
+    <!-- Estilos customizados para esse template -->
+    <link href="/css/signin.css" rel="stylesheet">
+  </head>
 
-    <body class="text-center">
+  <body class="text-center">
         <form class="form-signin" method="POST" action="">
         <img class="mb-2" src="/img/Logo.png" alt="">
         <h1 class="h3 mb-3 font-weight-normal">Faça seu cadastro</h1>
@@ -53,7 +31,7 @@
         <label for="inputText" class="sr-only">Genêro musical</label>
         <input type="text" name="genero_musical" id="inputText" class="form-control" placeholder="Genêro musical" required>
         <label for="inputText" class="sr-only">Telefone</label>
-        <input type="number" name="nome" id="inputNumber" class="form-control" placeholder="Telefone" required>
+        <input type="number" name="telefone" id="inputNumber" class="form-control" placeholder="Telefone" required>
         <label for="inputNumber" class="sr-only">Cachê do show</label>
         <input type="number" name="cache_show" id="inputNumber" class="form-control" placeholder="Seu cachê" required autofocus>
         <label for="inputPassword" class="sr-only">Senha</label>
@@ -62,3 +40,27 @@
         </form>
     </body>
 </html>
+<?php
+    include("conexao.php");
+    if(isset($_POST)){
+        $cpf_cnpj = $_POST['cpf_cnpj'];
+        $nome_fantasia = $_POST['nome_fantasia'];
+        $email = $_POST['email'];
+        $endereco = $_POST['endereco'];
+        $telefone = $_POST['telefone'];
+        $data_entrada = NOW();
+        $genero_musical = $_POST['genero_musical'];
+        $cache_show = $_POST['cache_show'];
+        $senha = md5($_POST['senha']);
+
+        $sql_code = "INSERT INTO user_musico values ('$cpf_cnpj', '$nome_fantasia', 
+        '$email', '$endereco', '$telefone', '$genero_musical', '$data_entrada', '$cache_show', '$senha')";
+        
+        if(mysqli_query($conexao, $sql_code)){
+            echo"Usuário cadastrado com sucesso!";
+        }else{
+            echo "Erro" . mysqli_connect_error($conexao);
+        }
+        mysqli_close($conexao);
+    }
+?>
--- login/cadastroContra.php
+++ login/cadastroContra.php
@@ -1,25 +1,3 @@
-<?php
-    include("conexao.php");
-
-    $cpf_cnpj = $_POST['cpf_cnpj'];
-    $nome = $_POST['nome'];
-    $nome_empresa = $_POST['nome_empresa'];
-    $email = $_POST['email'];
-    $endereco = $_POST['endereco'];
-    $telefone = $_POST['telefone'];
-    $data_entrada = NOW();
-    $senha = md5($_POST['senha']);
-
-    $sql_code = "INSERT INTO user_contratante values ('$cpf_cnpj', '$nome', '$nome_empresa', 
-    '$email', '$endereco', '$telefone', '$data_entrada', '$senha')";
-    
-    if(mysqli_query($conexao, $sql_code)){
-        echo"Usuário cadastrado com sucesso!";
-    }else{
-        echo "Erro" . mysqli_connect_error($conexao);
-    }
-    mysqli_close($conexao);
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
@@ -31,7 +9,7 @@
     <title>Cadastro - Contratante</title>
 
     <!-- Principal CSS do Bootstrap -->
-    <link href="\bootstrap\dist\css\bootstrap.min.css" rel="stylesheet">
+    <link href="/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Estilos customizados para esse template -->
     <link href="/css/signin.css" rel="stylesheet">
@@ -48,14 +26,35 @@
       <label for="inputText" class="sr-only">Nome da empresa</label>
       <input type="text" name="nome_empresa" id="inputText" class="form-control" placeholder="Nome empresa" required>
       <label for="inputEmail" class="sr-only">Email</label>
+      <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email" required>
       <label for="inputText" class="sr-only">Endereço</label>
       <input type="text" name="endereco" id="inputText" class="form-control" placeholder="Endereço" required>
-      <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email" required>
       <label for="inputText" class="sr-only">Telefone</label>
-      <input type="number" name="nome" id="inputNumber" class="form-control" placeholder="Telefone" required>
+      <input type="number" name="telefone" id="inputNumber" class="form-control" placeholder="Telefone" required>
       <label for="inputPassword" class="sr-only">Senha</label>
       <input type="password" name="senha" id="inputPassword" class="form-control" placeholder="Senha" required>
       <button class="btn btn-lg btn-danger btn-block" type="submit">Cadastrar</button>
     </form>
   </body>
 </html>
+<?php
+    include("conexao.php");
+    $cpf_cnpj = $_POST['cpf_cnpj'];
+    $nome = $_POST['nome'];
+    $nome_empresa = $_POST['nome_empresa'];
+    $email = $_POST['email'];
+    $endereco = $_POST['endereco'];
+    $telefone = $_POST['telefone'];
+    $data_entrada = NOW();
+    $senha = md5($_POST['senha']);
+
+    $sql_code = "INSERT INTO user_contratante values ('$cpf_cnpj', '$nome', '$nome_empresa', 
+    '$email', '$endereco', '$telefone', '$data_entrada', '$senha')";
+        
+    if(mysqli_query($conexao, $sql_code)){
+      echo "Usuário cadastrado com sucesso!";
+    }else{
+      echo "Erro" . mysqli_connect_error($conexao);
+    }
+    mysqli_close($conexao);
+?>
