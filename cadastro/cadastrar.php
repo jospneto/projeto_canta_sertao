19bfddbf2166676f1369ba45f2214e4a7938e16b
jospneto <josenetopereira380@gmail.com>
2022-03-03 09:17:50 -0300

    Atualizações

--- cadastro/cadastrar.php
+++ cadastro/cadastrar.php
@@ -1,22 +1,26 @@
 <?php
     include("conexao.php");
 
-    $cpf_cnpj = $_POST['cpf_cnpj'];
-    $nome = $_POST['nome'];
-    $nome_empresa = $_POST['nome_empresa'];
-    $email = $_POST['email'];
-    $endereco = $_POST['endereco'];
-    $telefone = $_POST['telefone'];
-    $data_entrada = NOW();
-    $senha = $_POST['senha'];
+    if(isset($_POST)){
+        echo "Preencha os dados do formulário";
+    }else{     
+        $cpf_cnpj = $_POST['cpf_cnpj'];
+        $nome = $_POST['nome'];
+        $nome_empresa = $_POST['nome_empresa'];
+        $email = $_POST['email'];
+        $endereco = $_POST['endereco'];
+        $telefone = $_POST['telefone'];
+        $data_entrada = NOW();
+        $senha = $_POST['senha'];
 
-    $sql_code = "INSERT INTO user_contratante values ('$cpf_cnpj', '$nome', '$nome_empresa', 
-    '$email', '$endereco', '$telefone', '$data_entrada', '$senha')";
+        $sql_code = "INSERT INTO user_contratante values ('$cpf_cnpj', '$nome', '$nome_empresa', 
+        '$email', '$endereco', '$telefone', '$data_entrada', '$senha')";
 
-    if(mysqli_query($conexao, $sql_code)){
-        echo"Usuário cadastrado com sucesso!";
-    }else{
-        echo "Erro" . mysqli_connect_error($conexao);
+        if(mysqli_query($conexao, $sql_code)){
+            echo"Usuário cadastrado com sucesso!";
+        }else{
+            echo "Erro" . mysqli_connect_error($conexao);
+        }
+        mysqli_close($conexao);
     }
-    mysqli_close($conexao);
 ?>
