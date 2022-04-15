46332a943ab672b30b6a290bfaa344468c5c8125
jospneto <josenetopereira380@gmail.com>
2022-04-15 16:25:06 -0300

    Atualizações página inicial, mudança no layout

--- index.php
+++ index.php
@@ -8,43 +8,63 @@
   <title>Página Inicial</title>
 </head>
 <body>
-  <nav class="menu">
-    <img src="img/Logo.png">
-    <ul>
-      <li><a href="https://cantasertao.wordpress.com/">Contatos</a></li>
-      <li class="login">Login
-        <form class="loginAcess" action="/login/index.php" method="POST">
-          <input type="email" name="email" placeholder="Digite seu email"><br>
-          <input type="password" name="senha" placeholder="Digite sua senha"><br>
-          <input type="submit" class="butEnviar" value="Entrar">
-        </form>
-      </li>
-      <li class="cadastro"><a href="cadastroAcess.php">Cadastro</a></li>
-    </ul>
-  </nav>
-  <div class="conteudo">
-    <ul class="listaPesquisa">
-      <h2>Procure artistas para seu <br> evento de forma rápida e simples</h2>
-      <form action="" class="pesquisa">
-        <img src="./img/Search.png" alt=""><input type="text" name="pesquisa" placeholder="Pesquisar músicos">
-      </form>
-    </ul>
-    <ul>
-      <li><img src="/img/cadastro.png" alt=""></li>
-      <li><img src="img/gratis.png" alt=""></li>
-    </ul>
-  </div>
-  <section class="artistas">
+  <section class="inicial">
+    <nav class="menu">
+      <img src="img/Logo.png">
       <ul>
-        <li><img src="" alt=""></li>
-        <h3>Artista1</h3>
-        <li><img src="" alt=""></li>
-        <h3>Artista1</h3>
-        <li><img src="" alt=""></li>
-        <h3>Artista1</h3>
-        <li><img src="" alt=""></li>
-        <h3>Artista1</h3>
+        <li><a href="https://cantasertao.wordpress.com/">Contatos</a></li>
+        <li class="login">Login
+          <form class="loginAcess" action="/login/index.php" method="POST">
+            <input type="email" name="email" placeholder="Digite seu email"><br>
+            <input type="password" name="senha" placeholder="Digite sua senha"><br>
+            <input type="submit" class="butEnviar" value="Entrar">
+          </form>
+        </li>
+        <li class="cadastro"><a href="cadastroAcess.php">Cadastro</a></li>
       </ul>
+    </nav>
+    <div class="conteudo">
+      <ul class="listaPesquisa">
+        <h2>Procure artistas para seu <br> evento de forma rápida e simples</h2>
+        <form action="" class="pesquisa">
+          <img src="./img/Search.png" alt=""><input type="text" name="pesquisa" placeholder="Pesquisar músicos">
+        </form>
+      </ul>
+      <ul>
+        <li><img src="/img/cadastro.png" alt=""></li>
+        <li><img src="img/gratis.png" alt=""></li>
+      </ul>
+    </div>
+  </section>
+  <section class="artistas">
+    <section class="generos">
+      <h3>GENÊROS</h3>
+      <ul>
+        <li>Forró</li>
+        <li>Rock</li>
+        <li>Pop</li>
+        <li>Sertanejo</li>
+      </ul>
+    </section>
+    <h1>ARTISTAS RECEM CADASTRADOS</h1>
+    <ul>
+        <li><img src="./img/artistas/img1.png" alt=""><h3>Artista1</h3></li>
+        <li><img src="./img/artistas/img2.png" alt=""><h3>Artista1</h3></li>
+        <li><img src="./img/artistas/img3.png" alt=""><h3>Artista1</h3></li>
+        <li><img src="./img/artistas/img4.png" alt=""><h3>Artista1</h3></li>
+    </ul>
+    <ul>
+        <li><img src="./img/artistas/img1.png" alt=""><h3>Artista1</h3></li>
+        <li><img src="./img/artistas/img2.png" alt=""><h3>Artista1</h3></li>
+        <li><img src="./img/artistas/img3.png" alt=""><h3>Artista1</h3></li>
+        <li><img src="./img/artistas/img4.png" alt=""><h3>Artista1</h3></li>
+    </ul>
+    <ul>
+        <li><img src="./img/artistas/img1.png" alt=""><h3>Artista1</h3></li>
+        <li><img src="./img/artistas/img2.png" alt=""><h3>Artista1</h3></li>
+        <li><img src="./img/artistas/img3.png" alt=""><h3>Artista1</h3></li>
+        <li><img src="./img/artistas/img4.png" alt=""><h3>Artista1</h3></li>
+    </ul>
   </section>
   <?php
     include("rodape.php");
